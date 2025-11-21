7546582644d91bd247c0a6fa25f64656ee262a84
Alejandra Duarte <aleflyduarte@gmail.com>
2025-11-20 21:06:46 -0300

    agregue algo

--- tarea1.py
+++ tarea1.py
@@ -3,4 +3,4 @@ print("Hola mundo Git!")
 print("Esta es una nueva versión")
 
 a = 1
-
+print("Hola mundo, tengo hambre") #puede la persona de abajo mandarme un sandwich?, esto no lo escribió la ale.
